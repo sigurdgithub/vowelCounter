762149b391a57d3dde90822e72492d40186afae5
Sigurd B <sigurdboels96@hotmail.com>
2022-09-07 12:22:55 +0200

    Show vowels arr

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ const vowels = ['A', 'E', 'I', 'O', 'U', 'Y'];
 // Get the vowels from the text inside the 'vowelBox'
 const countVowels = () => {
 	let counter = 0;
+	let countedVowels = [];
 	const textValue = document.getElementById('vowelBox').value;
 	// Use for of loop to iterate through str of textbox
 	for (let letter of textValue.toUpperCase()) {
 		if (vowels.includes(letter)) {
 			counter++;
+			countedVowels.push(letter);
 		}
 	}
+
+	//console.log(countedVowels);
 	return (document.getElementById(
 		'vowelCounter'
-	).innerHTML = `The number of vowels in your text equals ${counter}`);
+	).innerHTML = `The number of vowels in your text equals ${counter}<br>
+	and include the following:  ${countedVowels}`);
 
 	//document.getElementById('vowelCounter').innerHTML = typeof textValue;
 };
