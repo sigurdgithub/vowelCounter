5e87585d44f7e8ed539f20b197bc3dd7725f9f0f
Sigurd B <sigurdboels96@hotmail.com>
2022-09-07 12:07:18 +0200

    Counting of vowels

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 'use strict';
 // Reference the 'vowelBox' and detect its text
-const vowels = 'aeiouyAEIOUY';
+// Use array
+const vowels = ['A', E, 'I', 'O', 'U', 'Y'];
 // Get the vowels from the text inside the 'vowelBox'
-const countVowels = (vowels) => {
+const countVowels = () => {
+	let counter = 0;
 	const textValue = document.getElementById('vowelBox').value;
-	document.getElementById('vowelCounter').innerHTML = `${textValue}`;
+	// Use for of loop to iterate through str of textbox
+	for (let letter of textValue.toUpperCase()) {
+		if (vowels.includes(letter)) {
+			counter++;
+		} else {
+			return (document.getElementById('vowelCounter').innerHTML = `${counter}`);
+		}
+	}
+
+	//document.getElementById('vowelCounter').innerHTML = typeof textValue;
 };
 
 // Uppercase all vowels inside the array
